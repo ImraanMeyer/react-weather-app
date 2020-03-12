f9b6e6b2835c6e4d5d4f062a84af1f4503101d83
ImraanMeyer <imraan.meyer97@gmail.com>
2020-03-12 16:29:46 +0200

    Updates

---
+++ precache-manifest.08ddcd34818f150230f79b80e55979aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8715878e414c2c2c10ab8ad53a70987",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "77f1c793210da8728218",
+    "url": "/react-weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b967d7db8d3d62e75f0f",
+    "url": "/react-weather-app/static/css/main.f680d599.chunk.css"
+  },
+  {
+    "revision": "77f1c793210da8728218",
+    "url": "/react-weather-app/static/js/2.4ff8310a.chunk.js"
+  },
+  {
+    "revision": "b967d7db8d3d62e75f0f",
+    "url": "/react-weather-app/static/js/main.676f1697.chunk.js"
+  },
+  {
+    "revision": "87e20d0fc632d2fbd872",
+    "url": "/react-weather-app/static/js/runtime~main.6001fc69.js"
+  },
+  {
+    "revision": "36a919d9fa87720cc15df64f793fd7b3",
+    "url": "/react-weather-app/static/media/81744.36a919d9.jpg"
+  }
+]);
